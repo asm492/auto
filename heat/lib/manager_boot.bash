ea2e0cb9ab33f9bab7ee5bcc4143fd62ac580d7f
Monastyr <60261397+Monastyr@users.noreply.github.com>
2021-02-05 15:07:54 +0100

    Update manager_boot.bash

--- heat/lib/manager_boot.bash
+++ heat/lib/manager_boot.bash
@@ -97,7 +97,7 @@ chmod 500 /etc/ansible/key
 cd ~/Desktop
 discord.sh/./discord.sh --webhook-url="https://discord.com/api/webhooks/806168148922466304/6OMF-RpDB8XIQ9X-lebXgCnuoufB5h322IxZjKo4JB7kL7cGnxcBUL82Y3zYHbdB3Hqt" --text "Installerer DVVWA!"
 git clone https://github.com/Monastyr/ansible-ubuntu.git
-mv dvwa-ansible/* /etc/ansible/
+mv ansible-ubuntu/* /etc/ansible/
 cd /etc/ansible
 ansible-playbook main.yml
 ansible-playbook install_windows.yaml
