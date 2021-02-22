cff0164ea19c3f5332c4a04b622aca1270f52015
avleensm <asm492@gmail.com>
2021-02-22 21:09:35 +0100

    User now enteres target IP(s) into txt file

--- scanner.py
+++ scanner.py
@@ -1,6 +1,8 @@
 import nmap3
 import json
 import socket
+import sys
+import os
 import time
 import imgkit
 try:
@@ -8,14 +10,35 @@ try:
 except ImportError:
        print("screenshot module not found!")
 
+TARGETFILE = "target.txt"
 #target = "192.168.1.0/24"
-#target = "192.168.1.5"
 
 def exclude_self():
         host_ip = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
         f = open("exclude_ip.txt", "w")
         f.write(host_ip + "\n")
+        f.close()
 
+def has_target():
+        #Checks if target.txt exists and is populated
+   try:
+        f = open("target.txt","r")
+   except FileNotFoundError as e:
+        print(e)
+        write_log("\tTarget file is missing...quitting!")
+        print("\tTarget file is missing...quitting!")
+        sys.exit(1)
+   else:
+        if os.path.getsize(TARGETFILE):
+         #Ergo file is not empty
+           write_log("\tTarget file OK")
+           return 0
+        else:
+          #File empty
+          write_log("\tTarget is empty...quitting!")
+          sys.exit(1)
+
+        f.close()
 def write_log(text):
         logfile = open("log.txt","a")
         logfile.write(text + " at " + time.ctime() + "\n")
@@ -79,11 +102,12 @@ def possible_webpage(res):
 
 def perform_tcp_scan():
         #Aka Stage 2
-        #Screengrabs kommer her
-        print("\t[TCP SCAN] started")
+        #Tid uten -A på 192.168.1.0/24 og .2.0/24
+        #: 3:10. Med -A: 4:10 og det klikker.
+        # Med O: 3:36
         write_log("\t[TCP SCAN] started")
         nmap = nmap3.Nmap()
-        result = nmap.nmap_version_detection(None, "-sV -p- -iL ips_to_scan.txt");
+        result = nmap.nmap_version_detection(None, "-sV -p- -O -iL ips_to_scan.txt");
         print(result)
         possible_webpage(result)
         print_json_file(result, "tcp.json")
@@ -102,14 +126,16 @@ def perform_udp_scan():
 write_log("[SCRIPT] started")
 
 #MAIN:
-
+has_target()
 exclude_self()
 perform_host_discovery()
 result = perform_portscan()
 result = perform_tcp_scan()
 result = perform_udp_scan()
 
+
 #print(result)
+
 #Stage 4
 write_log("\t[SCREENGRABS] started")
 resp = screenshot.perform_screenshot()
