6b0ad560a169e0447bfc356de914095a4d3a2ed6
avleensm <asm492@gmail.com>
2021-03-11 16:13:49 +0100

    Test compose

--- Docker/website/server.js
+++ Docker/website/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const hostsRouter = require('./routes/hosts')
 const express = require('express')
 const app = express()
@@ -8,20 +9,10 @@ app.set('view engine', 'ejs')
 
 app.get('/', function (req, res){
     //Link til views/index.ejs
-    const hosts = [{
-      id: "156465846",
-      title: '192.168.1.6',
-      createdAt: new Date(),
-      description: 'Test host description'
-    },
-    {
-      id: "5548455466",
-      title: '192.168.1.5',
-      createdAt: new Date(),
-      description: 'Second host'
-     }]
     res.render('index', {hosts: hosts})
 });
+
+app.get('/all')
 
 app.use('/hosts', hostsRouter)
 
