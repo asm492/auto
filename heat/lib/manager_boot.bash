5639a440594630e947a7698e2b856bd18d413585
Monastyr <60261397+Monastyr@users.noreply.github.com>
2021-04-15 11:59:32 +0200

    Update manager_boot.bash

--- heat/lib/manager_boot.bash
+++ heat/lib/manager_boot.bash
@@ -83,8 +83,8 @@ chmod 500 /etc/ansible/key
 
 cd ~
 discord.sh/./discord.sh --webhook-url="https://discord.com/api/webhooks/806168148922466304/6OMF-RpDB8XIQ9X-lebXgCnuoufB5h322IxZjKo4JB7kL7cGnxcBUL82Y3zYHbdB3Hqt" --text "Installerer DVVWA!"
-git clone https://github.com/Monastyr/ansible-ubuntu.git
-mv ansible-ubuntu/* /etc/ansible/
+git clone https://github.com/Monastyr/autoenum-TestENV.git
+mv autoenum-TestENV/* /etc/ansible/
 cd /etc/ansible
 ~/discord.sh/./discord.sh --webhook-url="https://discord.com/api/webhooks/806168148922466304/6OMF-RpDB8XIQ9X-lebXgCnuoufB5h322IxZjKo4JB7kL7cGnxcBUL82Y3zYHbdB3Hqt" --text "Kjører playbooks"
 ansible-playbook main.yml
